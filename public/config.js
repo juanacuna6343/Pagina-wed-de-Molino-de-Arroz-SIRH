b3108a7e7e74b9b779c082886e60cb8e15ab9988
juanacuna6343 <juan.acuna6343@gmail.com>
2025-10-15 10:06:41 -0500

    Proyecto SIRH completo con frontend y backend

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
-// Configuración de Firebase
+// Configuración de Firebase para el frontend
 export const firebaseConfig = {
   apiKey: "AIzaSyDIxP8wdQ8aEEzTR1PWzVLq5Hueo3QnD98",
   authDomain: "proyecto1-1dc6c.firebaseapp.com",
@@ -8,8 +8,3 @@ export const firebaseConfig = {
   appId: "1:288402890139:web:0c81d536995d0afd00d53e",
   measurementId: "G-36P132HW77"
 };
-
-// URL base de la API - Apuntando a Render para el backend
-export const API_BASE_URL = window.location.hostname === 'localhost' 
-  ? 'http://localhost:3000/api' 
-  : 'https://sirh-molino-arroz.onrender.com/api';
